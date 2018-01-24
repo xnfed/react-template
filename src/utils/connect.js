27dc6939a9178fce86239950d4287c29969c4917
xugaobo <xugaobo@jd.com>
2018-01-24 19:51:02 +0800

    修改装饰器

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import * as actions from '../app/actions'
 
 function mapStateToProps(state) {
     return state
@@ -8,15 +7,12 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        actions: bindActionCreators(actions, dispatch)
+        actions: bindActionCreators(function() {}, dispatch)
     }
 }
 
 export default (state) => {
     return (target) => {
-        // target.prototype.setTitle = (title) => {
-        //     document.title = title
-        // }
         return connect(mapStateToProps, mapDispatchToProps)(target)
     }
 }
